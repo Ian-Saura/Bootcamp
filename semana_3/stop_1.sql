afdfae8002836364f223ace0d6c6021d85e5b1f5
Ian Saura <iansaura@cookunity.com>
2025-10-31 18:53:38 -0300

    feat: update SQL query to use attached ventas database and modify file references

--- semana_3/stop_1.sql
+++ semana_3/stop_1.sql
@@ -1,8 +1,15 @@
+ATTACH DATABASE 'semana_3/database/ventas.duckdb' AS ventas_db;
+
+
+
 SELECT *
-FROM ventas
+FROM ventas_db.ventas
 WHERE fecha BETWEEN '2024-01-01' AND '2024-03-31'
   AND cantidad >= 5
   ORDER BY fecha;
 
 
-#duckdb semana_3/ventas.duckdb < semana_3/stop_2.sql
+#duckdb < semana_3/stop_1.sql
+
+Argentina
+Argentina
